dc695f0ae649140bbc59b3cd5459843e91167d23
William del Rosario <williamdelrosario@yahoo.com>
2018-02-24 15:17:06 +0100

    Api validation initialized.

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,46 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Given I am an anonymous user
+     */
+    public function iAmAnAnonymousUser()
+    {
+        return true;
+    }
+
+    /**
+     * @When I search for behat
+     */
+    public function iSearchForBehat()
+    {
+        return false;
+    }
+
+    /**
+     * @Then I get a result
+     */
+    public function iGetAResult()
+    {
+        throw new Exception();
+    }
+}
